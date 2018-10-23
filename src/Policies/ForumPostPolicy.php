64889c73d0109367c7844be774100ed9dca3c2ed
marco76tv <marco.sottana@gmail.com>
2018-10-23 18:05:02 +0200

    some policy

---
+++ src/Policies/ForumPostPolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace XRA\Forum\Policies;
+/*
+use App\User;
+use App\Post;
+*/
+use XRA\LU\Models\User;
+use XRA\Forum\Models\ForumCat as Post;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ForumPostPolicy{
+    use HandlesAuthorization;
+
+    public function before($user, $ability){
+        if (isset($user->perm) && $user->perm->perm_type>=5) {  //superadmin
+            return true;
+        }
+    }
+
+    /*
+    public function update(User $user, Post $post)
+    {
+        return $user->id === $post->user_id;
+    }
+    */
+
+    public function create(User $user, Post $post){
+        return true;
+    }
+
+    public function edit(User $user, Post $post){
+        if($post->created_by==$user->handle  ){
+            return true;
+        }
+        return false;
+    }
+
+    public function show(User $user, Post $post){
+        return true;
+    }
+}
